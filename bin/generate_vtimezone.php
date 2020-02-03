d728b8aa8533cd56c13aea3c845b27a5a41faccc
Phil Davis <phil@jankaritech.com>
2020-02-03 13:36:47 +0545

    Standardize CI 20200203

--- bin/generate_vtimezone.php
+++ bin/generate_vtimezone.php
@@ -4,24 +4,23 @@
 namespace Sabre\TzServer;
 
 $inputFiles = [
-    "africa",
-    "antarctica",
-    "asia",
-    "australasia",
-    "europe",
-    "northamerica",
-    "southamerica",
+    'africa',
+    'antarctica',
+    'asia',
+    'australasia',
+    'europe',
+    'northamerica',
+    'southamerica',
 ];
 
-$path = __DIR__ . '/tzdata/';
+$path = __DIR__.'/tzdata/';
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 $parser = new TzDataParser\Parser();
 
-foreach($inputFiles as $inputFile) {
-    $parser->parseFile($path . $inputFile);
-
+foreach ($inputFiles as $inputFile) {
+    $parser->parseFile($path.$inputFile);
 }
 
 $vtimezoneGenerator = new TzDataParser\VTimeZoneGenerator($parser);
