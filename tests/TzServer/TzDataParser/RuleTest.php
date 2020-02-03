e3c490c7d1d199c883480f6db4d6abe5fea9d09c
Phil Davis <phil@jankaritech.com>
2020-02-03 13:36:47 +0545

    Standardize CI 20200203

--- tests/TzServer/TzDataParser/RuleTest.php
+++ tests/TzServer/TzDataParser/RuleTest.php
@@ -6,62 +6,57 @@ use DateTime;
 use DateTimeZone;
 use PHPUnit\Framework\TestCase;
 
-class RuleTest extends TestCase {
-
-    function getRule($yearUntil = 'only') {
-
+class RuleTest extends TestCase
+{
+    public function getRule($yearUntil = 'only')
+    {
         $zoneLine = new ZoneLine([
             'gmtoff' => '-6:00',
-            'rules'  => 'Chicago',
+            'rules' => 'Chicago',
             'format' => 'C%sT',
             'until   => 1963 Mar  1 2:00',
-        ]); 
+        ]);
 
         $rule = new Rule([
-            'name'   => 'Chicago',
-            'from'   => '1920',
-            'to'     => $yearUntil,
-            'type'   => '-',
-            'in'     => 'Jun',
-            'on'     => '13',
-            'at'     => '2:00',
-            'save'   => '1:00',
+            'name' => 'Chicago',
+            'from' => '1920',
+            'to' => $yearUntil,
+            'type' => '-',
+            'in' => 'Jun',
+            'on' => '13',
+            'at' => '2:00',
+            'save' => '1:00',
             'letter' => 'D',
         ]);
         $rule->zoneContext = $zoneLine;
 
         return $rule;
-
     }
 
-    function testGetStartTime() {
-
+    public function testGetStartTime()
+    {
         $rule = $this->getRule();
         $time = $rule->getStartTime();
 
         $expected = new DateTime('1920-06-13 02:00', new DateTimeZone('-06:00'));
-        $this->assertEquals($expected->getTimeStamp(), $time, 'Got: ' . gmdate(DATE_ATOM, $time));
-
+        $this->assertEquals($expected->getTimeStamp(), $time, 'Got: '.gmdate(DATE_ATOM, $time));
     }
 
-    function testGetEndTimeOnly() {
-
+    public function testGetEndTimeOnly()
+    {
         $rule = $this->getRule();
         $time = $rule->getStartTime();
 
         $expected = new DateTime('1920-06-13 02:00', new DateTimeZone('-06:00'));
-        $this->assertEquals($expected->getTimeStamp(), $time, 'Got: ' . gmdate(DATE_ATOM, $time));
-
+        $this->assertEquals($expected->getTimeStamp(), $time, 'Got: '.gmdate(DATE_ATOM, $time));
     }
 
-    function testGetEndTime() {
-
+    public function testGetEndTime()
+    {
         $rule = $this->getRule('1960');
         $time = $rule->getStartTime();
 
         $expected = new DateTime('1920-06-13 02:00', new DateTimeZone('-06:00'));
-        $this->assertEquals($expected->getTimeStamp(), $time, 'Got: ' . gmdate(DATE_ATOM, $time));
-
+        $this->assertEquals($expected->getTimeStamp(), $time, 'Got: '.gmdate(DATE_ATOM, $time));
     }
-
 }
